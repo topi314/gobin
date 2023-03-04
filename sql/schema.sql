be4f294fc290f2ed601158a9088b81bbe4af2862
ToπSenpai <git@topi.wtf>
2023-03-05 00:20:32 +0100

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS documents
 (
-    id           VARCHAR   NOT NULL,
-    version      TIMESTAMP NOT NULL,
-    content      TEXT      NOT NULL,
-    language     VARCHAR   NOT NULL,
+    id       VARCHAR NOT NULL,
+    version  BIGINT  NOT NULL,
+    content  TEXT    NOT NULL,
+    language VARCHAR NOT NULL,
     PRIMARY KEY (id, version)
 );
