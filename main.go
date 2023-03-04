f048a467e429d923de652f3fc40470f0f6788118
TopiSenpai <git@topi.wtf>
2023-03-04 16:15:03 +0100

    update docs & add default values

--- main.go
+++ main.go
@@ -36,10 +36,26 @@ var (
 )
 
 func main() {
-	log.Printf("Gobin version: %s (commit: %s, build time: %s)", version, commit, buildTime)
-
+	log.Printf("Starting Gobin with version: %s (commit: %s, build time: %s)...", version, commit, buildTime)
 	cfgPath := flag.String("config", "", "path to gobin.json")
 	flag.Parse()
+
+	viper.SetDefault("listen_addr", ":80")
+	viper.SetDefault("dev_mode", false)
+	viper.SetDefault("database.type", "sqlite")
+	viper.SetDefault("database.debug", false)
+	viper.SetDefault("database.expire_after", "0")
+	viper.SetDefault("database.cleanup_interval", "1m")
+	viper.SetDefault("database.path", "gobin.db")
+	viper.SetDefault("database.host", "localhost")
+	viper.SetDefault("database.port", 5432)
+	viper.SetDefault("database.username", "gobin")
+	viper.SetDefault("database.password", "password")
+	viper.SetDefault("database.database", "gobin")
+	viper.SetDefault("database.ssl_mode", "disable")
+	viper.SetDefault("max_document_size", 0)
+	viper.SetDefault("rate_limit.requests", 10)
+	viper.SetDefault("rate_limit.duration", "1m")
 
 	viper.SetConfigName("gobin")
 	viper.SetConfigType("json")
@@ -51,7 +67,8 @@ func main() {
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalln("Error while reading config:", err)
 	}
-	log.Printf("Gobin starting... (config path:%s)", *cfgPath)
+	viper.SetEnvPrefix("gobin")
+	viper.AutomaticEnv()
 
 	var cfg gobin.Config
 	if err := viper.Unmarshal(&cfg, func(config *mapstructure.DecoderConfig) {
